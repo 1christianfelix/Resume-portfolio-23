203d941a288f1d5ebd842e8c6e2c467a26993319
Christian Felix <1christianfelix@gmail.com>
2023-05-11 08:42:03 -0700

    changed sticky note

--- src/components/Sticky.jsx
+++ src/components/Sticky.jsx
@@ -11,9 +11,12 @@ const Sticky = () => {
     >
       <div className="h-60 w-60 bg-[#feff9c] absolute z-[90] p-3">
         <span className="">Reminders:</span>
-        <ul className="pt-2">
+        <ul className="pt-2 text-sm">
           <li>1.Fix the media queires for everything under 1080p!</li>
           <li>2.Double click or Single click? hmmm</li>
+          <li className="mb-5"></li>
+          <li>Want to ask me stuff???</li>
+          <li>Contact me here! 1christianfelix@gmail.com</li>
         </ul>
       </div>
     </Draggable>
